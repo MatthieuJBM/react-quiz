b96d97c41f3beb563e4d00957386545f98a66f4c
Mateusz Baryla <m.baryla@protonmail.ch>
2023-08-09 12:19:30 +0200

    Finish screen displaying the score added

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import { useEffect, useReducer } from "react";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -49,6 +50,8 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return { ...state, status: "finished" };
     default:
       throw new Error("Action unknown");
   }
@@ -96,8 +99,16 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
         )}
       </Main>
     </div>
